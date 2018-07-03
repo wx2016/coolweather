52d3ed16c00c1d8eb9b39247bdc16aced6d5e804
wangxuan17 <wangxuan17@baidu.com>
2018-07-03 17:21:14 +0800

    add on feature-2 branch

--- app/src/main/java/com/example/wangxuan17/gitdemo/MainActivity.java
+++ app/src/main/java/com/example/wangxuan17/gitdemo/MainActivity.java
@@ -2,15 +2,20 @@ package com.example.wangxuan17.gitdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private TextView textView;
+    private Button btn2;
+    private Button btn3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView = (TextView)findViewById(R.id.text);
+        btn2 = (Button)findViewById(R.id.btn2);
+        btn3 = (Button)findViewById(R.id.btn3);
     }
 }
