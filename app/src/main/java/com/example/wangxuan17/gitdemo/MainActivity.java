fdd3d143e54253c0ce8b37371a56e55e96c7535c
wangxuan17 <wangxuan17@baidu.com>
2018-07-03 17:13:23 +0800

    add on feature-1 branch

--- app/src/main/java/com/example/wangxuan17/gitdemo/MainActivity.java
+++ app/src/main/java/com/example/wangxuan17/gitdemo/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.wangxuan17.gitdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView)findViewById(R.id.text);
     }
 }
